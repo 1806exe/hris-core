70fe16e85163ea067d7216b51beb4f4752ecb576
Walter V. Faustine <waltervfaustine@gmail.com>
2020-05-10 11:38:53 +0300

    fix: 🐛 updated field module to use updated base controller
    This commit will update field module to use updated base controller
    which is maintenance controller

--- src/modules/form/controllers/field.controller.ts
+++ src/modules/form/controllers/field.controller.ts
@@ -3,9 +3,10 @@ import { BaseController } from 'src/core/controllers/base.contoller';
 
 import { Field } from '../entities/field.entity';
 import { FieldService } from '../services/field.service';
+import { MaintenanceBaseController } from 'src/core/maintenance/controllers/base.contoller';
 
 @Controller('api/' + Field.plural)
-export class FieldController extends BaseController<Field> {
+export class FieldController extends MaintenanceBaseController<Field> {
   constructor(fieldService: FieldService) {
     super(fieldService, Field);
   }
