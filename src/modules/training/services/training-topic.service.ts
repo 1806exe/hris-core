a010312811112f73f86a226369ff81f299d4ecc8
Walter V. Faustine <waltervfaustine@gmail.com>
2020-05-12 15:29:51 +0300

    fix: 🐛 updated training module
    This commit will add maintenance service and maintenance controller in
    training module

--- src/modules/training/services/training-topic.service.ts
+++ src/modules/training/services/training-topic.service.ts
@@ -3,9 +3,10 @@ import { BaseService } from 'src/core/services/base.service';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TrainingTopic } from '../entities/training-topic.entity';
+import { MaintenanceBaseService } from 'src/core/maintenance/services/base.service';
 
 @Injectable()
-export class TrainingTopicService extends BaseService<TrainingTopic> {
+export class TrainingTopicService extends MaintenanceBaseService<TrainingTopic> {
     constructor(
         @InjectRepository(TrainingTopic)
         trainingMethodRepository: Repository<TrainingTopic>,
