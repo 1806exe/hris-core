cfdaeddf0b21bed9c9aa7187b8b25bf9d7e6cff5
Walter V. Faustine <waltervfaustine@gmail.com>
2020-05-12 15:31:51 +0300

    fix: 🐛 updated forms module
    This commit will add maintanance service and maintenance controller in
    forms module

--- src/modules/form/services/form.service.ts
+++ src/modules/form/services/form.service.ts
@@ -3,9 +3,10 @@ import { Form } from '../entities/form.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseService } from 'src/core/services/base.service';
+import { MaintenanceBaseService } from 'src/core/maintenance/services/base.service';
 
 @Injectable()
-export class FormService extends BaseService<Form> {
+export class FormService extends MaintenanceBaseService<Form> {
     constructor(
         @InjectRepository(Form)
         repository: Repository<Form>,
