6d05af919c3b818e55ee8ecf029c6950bbaba0c1
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-04 08:51:56 +0300

    fix: 🐛 updated form field members in form module
    This commit will update form field members in entity, service and
    controller

--- src/modules/form/services/form-field-member.service.ts
+++ src/modules/form/services/form-field-member.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { FormFieldMember } from '../entities/form-field-member.entity';
-import { BaseService } from 'src/core/services/base.service';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { MaintenanceBaseService } from 'src/core/maintenance/services/base.service';
 
 @Injectable()
-export class FormFieldMemberService extends BaseService<FormFieldMember> {
+export class FormFieldMemberService extends MaintenanceBaseService<FormFieldMember> {
     constructor(
         @InjectRepository(FormFieldMember)
         repository: Repository<FormFieldMember>,
