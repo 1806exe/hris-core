c4acda0a19ec01f2e9eb290aa914cc4c355a65ab
Bennett <bennybenester@gmail.com>
2020-06-10 12:48:31 +0300

    feat: move access entities to user and userrole modules

---
+++ src/modules/system/user/entities/user-access.entity.ts
@@ -0,0 +1,29 @@
+import { UserIdentification } from './user-identification';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  JoinColumn,
+  ManyToMany,
+} from 'typeorm';
+import { User } from './user.entity';
+import { TrainingSession } from '../../../training/entities/training-session.entity';
+@Entity('useraccess', { schema: 'public' })
+export class UserAccess extends UserIdentification {
+  static plural = 'userAccesses';
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('json', {
+    nullable: false,
+    name: 'useraccess',
+  })
+  access: any;
+
+  @Column('integer', { nullable: false, name: 'userid' })
+  userid: number;
+
+  @ManyToMany((type) => TrainingSession, (session) => session.access)
+  session: TrainingSession;
+}
