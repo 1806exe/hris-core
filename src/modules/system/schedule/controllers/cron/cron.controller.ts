00534a8294457a53df32ccfde2e3a762746d4fc4
Walter V. Faustine <waltervfaustine@gmail.com>
2020-07-04 14:39:01 +0300

    feat: 🎸 added support for crop API endpoint
    This commit will add support for crop API endpoint

---
+++ src/modules/system/schedule/controllers/cron/cron.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { MaintenanceBaseController } from '@hris/core/maintenance/controllers/base.controller';
+import { Cron } from '../../entities/cron.entity';
+import { CronService } from '../../services/cron/cron.service';
+
+@Controller('api/' + Cron.plural)
+export class CronController extends MaintenanceBaseController<Cron> {
+    constructor(cronService: CronService) {
+        super(cronService, Cron);
+    }
+}
---
+++ src/modules/system/schedule/entities/cron.entity.ts
@@ -0,0 +1,48 @@
+import { EntityCoreProps } from '@hris/core/entities/entity-core-props';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import { TransactionTimestamp } from '../../../../core/entities/transaction-timestamp.entity';
+import { generateUid } from '../../../../core/helpers/makeuid';
+
+@Entity('cron', { schema: 'public' })
+export class Cron extends TransactionTimestamp {
+    static plural = 'crons';
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 256, unique: true })
+    uid: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 255,
+    })
+    name: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 255,
+    })
+    description: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 255,
+    })
+    cron: string;
+
+    @BeforeInsert()
+    beforeInsertTransactionTimestamp() {
+        /**
+         *  You can generate UUID in different ways
+         *  1. You can generate uuid of any length: i.e getUid('', 20)
+         *      Example of UUID::: 8aydSxYBrrP
+         *  2. You can generat UUID by prepending a context specific keyword i.e getUid('HRIS', 20)
+         *      Example of UUID::: HRIS_8aydSxYBrrP
+         */
+        this.uid = this.uid || generateUid();
+    }
+}
