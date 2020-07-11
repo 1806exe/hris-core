9769931b61084535ac0baeae8dc1aab14faf3b4a
Bennett <bennybenester@gmail.com>
2020-07-11 14:58:01 +0300

    feat: add venue-orgunit migrations relationship

--- src/database/migration/1592483454024-indicators.ts
+++ src/database/migration/1592483454024-indicators.ts
@@ -202,10 +202,10 @@ UPDATE FIELD F SET DATATYPE = D.name
             ALTER TABLE public.form ADD CONSTRAINT UQ_b745636607c72a13191d0b91f77 UNIQUE (uid);
             ALTER TABLE trainingvenue ADD COLUMN organisationunit BIGINT;
 
-       ALTER TABLE trainingvenue UPDATE COLUMN organisationunit 
-       UPDATE trainingvenue t SET organisationunit = o.id
-        FROM  organisationunit o
-        WHERE o.name = t.district;
+            ALTER TABLE trainingvenue ADD COLUMN organisationunit BIGINT;
+            UPDATE trainingvenue t SET organisationunit = o.id
+            FROM  organisationunit o
+            WHERE o.name = t.district;
 `);
   }
   public async down(queryRunner: QueryRunner): Promise<any> {}
