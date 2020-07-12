e07d9dd70928447353adc95631d7db6d373960f7
Bennett <bennybenester@gmail.com>
2020-07-12 11:46:28 +0300

    feat: add endpoint to create venue

--- src/modules/training/controllers/training-venue.controller.ts
+++ src/modules/training/controllers/training-venue.controller.ts
@@ -1,14 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body } from '@nestjs/common';
 import { BaseController } from '../../../core/controllers/base.contoller';
 
 import { TrainingVenueService } from '../services/training-venue.service';
 import { TrainingVenue } from '../entities/training-venue.entity';
+import { SessionGuard } from '../../../modules/system/user/guards/session.guard';
 
 @Controller('api/training/' + TrainingVenue.plural)
-export class TrainingVenueController extends BaseController<
-TrainingVenue
-> {
-    constructor(trainingVenueService: TrainingVenueService) {
-        super(trainingVenueService, TrainingVenue);
-    }
+export class TrainingVenueController extends BaseController<TrainingVenue> {
+  constructor(private trainingVenueService: TrainingVenueService) {
+    super(trainingVenueService, TrainingVenue);
+  }
+  @Post()
+  @UseGuards(SessionGuard)
+  async createVenue(@Body() createVenueDTO) {
+    return this.trainingVenueService.createVenue(createVenueDTO);
+  }
 }
--- src/modules/training/services/training-venue.service.ts
+++ src/modules/training/services/training-venue.service.ts
@@ -3,13 +3,30 @@ import { BaseService } from '../../../core/services/base.service';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TrainingVenue } from '../entities/training-venue.entity';
+import { OrganisationUnit } from '../../../modules/organisation-unit/entities/organisation-unit.entity';
+import { generateUid } from '../../../core/helpers/makeuid';
 
 @Injectable()
 export class TrainingVenueService extends BaseService<TrainingVenue> {
-    constructor(
-        @InjectRepository(TrainingVenue)
-        trainingVenueRepository: Repository<TrainingVenue>,
-    ) {
-        super(trainingVenueRepository, TrainingVenue);
-    }
+  constructor(
+    @InjectRepository(TrainingVenue)
+    private trainingVenueRepository: Repository<TrainingVenue>,
+    @InjectRepository(OrganisationUnit)
+    private organisationUnitRepository: Repository<OrganisationUnit>,
+  ) {
+    super(trainingVenueRepository, TrainingVenue);
+  }
+  async createVenue(createvenueDTO: TrainingVenue): Promise<TrainingVenue> {
+    const { name, district, region, organisationUnit } = createvenueDTO;
+
+    return await this.trainingVenueRepository.save({
+      uid: generateUid(),
+      name: name,
+      district: district,
+      region: region,
+      organisationUnit: await this.organisationUnitRepository.findOne({
+        where: { uid: organisationUnit },
+      }),
+    });
+  }
 }
