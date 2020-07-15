3442f4290a4c1be092ed3be7ed82dad0a6f521e0
Bennett <bennybenester@gmail.com>
2020-07-15 14:42:26 +0300

    feat: add ability to get orgunit parents from filter

--- src/modules/organisation-unit/controllers/organisation-unit.controller.ts
+++ src/modules/organisation-unit/controllers/organisation-unit.controller.ts
@@ -1,14 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Query } from '@nestjs/common';
 
 import { OrganisationUnitService } from '../services/organisation-unit.service';
 import { OrganisationUnit } from '../entities/organisation-unit.entity';
 import { MaintenanceBaseController } from '../../../core/maintenance/controllers/base.controller';
+import { SessionGuard } from '@hris/modules/system/user/guards/session.guard';
+import { ApiResult, Pager } from '@hris/core/interfaces';
+import { sanitizeResponseObject } from '@hris/core/utilities/sanitize-response-object';
+import { getPagerDetails } from '@hris/core/utilities';
+import * as _ from 'lodash';
 
 @Controller('api/' + OrganisationUnit.plural)
 export class OrganisationUnitsController extends MaintenanceBaseController<
   OrganisationUnit
 > {
-  constructor(organisationUnitService: OrganisationUnitService) {
+  constructor(private organisationUnitService: OrganisationUnitService) {
     super(organisationUnitService, OrganisationUnit);
+  }
+  @Get()
+  @UseGuards(SessionGuard)
+  async findAll(@Query() query): Promise<ApiResult> {
+    if (_.has(query, 'paging') && query.paging === 'false') {
+      const allContents: OrganisationUnit[] = await this.organisationUnitService.findAll();
+      return {
+        [OrganisationUnit.plural]: _.map(allContents, sanitizeResponseObject),
+      };
+    } else if (_.has(query, 'name')) {
+      const foundName = await this.organisationUnitService.findOneByName(
+        query?.name,
+      );
+      return { [OrganisationUnit.plural]: foundName };
+    }
+    const pagerDetails: Pager = getPagerDetails(query);
+
+    const [entityRes, totalCount]: [
+      OrganisationUnit[],
+      number,
+    ] = await this.organisationUnitService.findAndCount(
+      query?.fields,
+      query?.filter,
+      pagerDetails?.pageSize,
+      +pagerDetails?.page - 1,
+    );
+    return {
+      pager: {
+        ...pagerDetails,
+        pageCount: entityRes?.length,
+        total: totalCount,
+        nextPage: `/api/${OrganisationUnit.plural}?page=${
+          +pagerDetails.page + +'1'
+        }`,
+      },
+      [OrganisationUnit.plural]: _.map(entityRes, sanitizeResponseObject),
+    };
   }
 }
--- src/modules/organisation-unit/services/organisation-unit.service.ts
+++ src/modules/organisation-unit/services/organisation-unit.service.ts
@@ -7,6 +7,13 @@ import { UIDToIDTransformation } from '@icodebible/utils/resolvers/uid-to-id';
 import { OrganisationUnit } from '../entities/organisation-unit.entity';
 import { MaintenanceBaseService } from '../../../core/maintenance/services/base.service';
 import { entityTableMapper } from '../../../core/resolvers/database-table.resolver';
+import {
+  getSelections,
+  getRelations,
+} from '../../../core/utilities/get-fields.utility';
+import { getWhereConditions } from '../../../core/utilities';
+import { GetResponseSanitizer } from '@icodebible/utils/resolvers/id-to-uid';
+import { fil } from 'date-fns/locale';
 
 @Injectable()
 export class OrganisationUnitService extends MaintenanceBaseService<
@@ -17,5 +24,70 @@ export class OrganisationUnitService extends MaintenanceBaseService<
     private organisationUnitRepository: Repository<OrganisationUnit>,
   ) {
     super(organisationUnitRepository, OrganisationUnit);
+  }
+  async findAndCount(
+    fields,
+    filter,
+    size,
+    page,
+  ): Promise<[OrganisationUnit[], number]> {
+    const metaData = this.organisationUnitRepository.manager.connection.getMetadata(
+      OrganisationUnit,
+    );
+    console.log('Fields', fields);
+    let join: any = {};
+
+    //TODO: Find best way to join any recursive relation
+    if (metaData.tableName === 'organisationunit') {
+      join = {
+        alias: 'organisationunit',
+        leftJoinAndSelect: {
+          profile: 'organisationunit.parent',
+        },
+      };
+    }
+    const parent = filter.includes('parent.id:eq:');
+    if (parent) {
+      const parents = await this.organisationUnitRepository.findOne({
+        where: { uid: filter.replace(/^parent.id:eq:+/i, '') },
+      });
+      const [response, totalCount] = await this.modelRepository.findAndCount({
+        select: getSelections(fields, metaData),
+        relations: getRelations(fields, metaData),
+        where: { parent: parents },
+        take: size,
+        join,
+        skip: page * size,
+      });
+      return [
+        await GetResponseSanitizer(
+          this.modelRepository,
+          response,
+          entityTableMapper,
+        ),
+        totalCount,
+      ];
+    }
+    const [response, totalCount] = await this.modelRepository.findAndCount({
+      select: getSelections(fields, metaData),
+      relations: getRelations(fields, metaData),
+      where: getWhereConditions(filter),
+      take: size,
+      join,
+      skip: page * size,
+    });
+
+    /**
+     *
+     */
+
+    return [
+      await GetResponseSanitizer(
+        this.modelRepository,
+        response,
+        entityTableMapper,
+      ),
+      totalCount,
+    ];
   }
 }
