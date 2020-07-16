2ea9df310f40f6717afadab069c82711ee82551e
Bennett <bennybenester@gmail.com>
2020-07-16 23:15:38 +0300

    refactor: increase default pagesize

--- src/core/utilities/get-pager-details.utility.ts
+++ src/core/utilities/get-pager-details.utility.ts
@@ -3,7 +3,7 @@ import { Pager } from '../interfaces/pager.interface';
 export function getPagerDetails(queryParams: any): Pager {
   return {
     page: queryParams ? queryParams.page || 1 : 1,
-    pageSize: queryParams ? queryParams.pageSize || 50 : 50,
+    pageSize: queryParams ? queryParams.pageSize || 100 : 100,
     pageCount: 0,
     total: 0,
     nextPage: '',
