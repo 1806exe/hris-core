e7cadbf2d007cbb490e838ffd63034164cc73b57
Bennett <bennybenester@gmail.com>
2020-07-16 22:33:07 +0300

    refactor: remove redundancy in getting parent and in filters

--- src/modules/organisation-unit/services/organisation-unit.service.ts
+++ src/modules/organisation-unit/services/organisation-unit.service.ts
@@ -13,7 +13,6 @@ import {
 } from '../../../core/utilities/get-fields.utility';
 import { getWhereConditions } from '../../../core/utilities';
 import { GetResponseSanitizer } from '@icodebible/utils/resolvers/id-to-uid';
-import { fil } from 'date-fns/locale';
 
 @Injectable()
 export class OrganisationUnitService extends MaintenanceBaseService<
@@ -45,11 +44,15 @@ export class OrganisationUnitService extends MaintenanceBaseService<
         },
       };
     }
-
+    //TODO: Softcode fetching to apply in all endpoints
     const [response, totalCount] = await this.modelRepository.findAndCount({
       select: getSelections(fields, metaData),
       relations: getRelations(fields, metaData),
-      where: getWhereConditions(filter),
+      where: filter && filter.includes('parent.id:eq')?{
+        parent: await this.organisationUnitRepository.findOne({
+          where: { uid: filter.replace(/^parent.id:eq:+/i, '') },
+        })
+      }:getWhereConditions(filter),
       take: size,
       join,
       skip: page * size,
@@ -68,89 +71,5 @@ export class OrganisationUnitService extends MaintenanceBaseService<
       totalCount,
     ];
   }
-
-  /*
-   * Get organisationunit parent filters based on find and count selections
-   */
-
-  async getParent(filter, size, page): Promise<any> {
-    const metaData = this.organisationUnitRepository.manager.connection.getMetadata(
-      OrganisationUnit,
-    );
-    let join: any = {};
-
-    //TODO: Find best way to join any recursive relation
-    if (metaData.tableName === 'organisationunit') {
-      join = {
-        alias: 'organisationunit',
-        leftJoinAndSelect: {
-          profile: 'organisationunit.parent',
-        },
-      };
-    }
-
-    const parents = await this.organisationUnitRepository.findOne({
-      where: { uid: filter.replace(/^parent.id:eq:+/i, '') },
-    });
-    const [
-      response,
-      totalCount,
-    ] = await this.organisationUnitRepository.findAndCount({
-      where: { parent: parents },
-      take: size,
-      join,
-      skip: page * size,
-    });
-
-    return [
-      await GetResponseSanitizer(
-        this.organisationUnitRepository,
-        response,
-        entityTableMapper,
-      ),
-      totalCount,
-    ];
-  }
-  async filterIn(filter, size, page): Promise<any> {
-    const metaData = this.organisationUnitRepository.manager.connection.getMetadata(
-      OrganisationUnit,
-    );
-    let join: any = {};
-
-    //TODO: Find best way to join any recursive relation
-    if (metaData.tableName === 'organisationunit') {
-      join = {
-        alias: 'organisationunit',
-        leftJoinAndSelect: {
-          profile: 'organisationunit.parent',
-        },
-      };
-    }
-    const [
-      response,
-      totalCount,
-    ] = await this.organisationUnitRepository.findAndCount({
-      where: {
-        uid: In(
-          /eq:(.+)/
-            .exec(filter)[1]
-            .slice(1, -1)
-            .split(',')
-            .map((filters) => filters),
-        ),
-      },
-      take: size,
-      join,
-      skip: page * size,
-    });
-
-    return [
-      await GetResponseSanitizer(
-        this.organisationUnitRepository,
-        response,
-        entityTableMapper,
-      ),
-      totalCount,
-    ];
-  }
+  
 }
