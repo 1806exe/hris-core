7a3b3090b1f0f5558e7fef762b7a046a56cfa131
Bennett <bennybenester@gmail.com>
2020-07-21 22:25:40 +0300

    style: implement endpoint to create a curriculum

--- src/modules/training/controllers/training-curriculum.controller.ts
+++ src/modules/training/controllers/training-curriculum.controller.ts
@@ -1,14 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Res, Body, HttpStatus } from '@nestjs/common';
 
 import { TrainingCurriculumService } from '../services/training-curriculum.service';
 import { TrainingCurriculum } from '../entities/training-curriculum.entity';
 import { MaintenanceBaseController } from '../../../core/maintenance/controllers/base.controller';
+import { SessionGuard } from '../../system/user/guards/session.guard';
+import { sanitizeResponseObject } from '../../../core/utilities/sanitize-response-object';
 
 @Controller('api/training/' + TrainingCurriculum.plural)
 export class TrainingCurriculumController extends MaintenanceBaseController<
   TrainingCurriculum
 > {
-  constructor(trainingCurriculumService: TrainingCurriculumService) {
+  constructor(private trainingCurriculumService: TrainingCurriculumService) {
     super(trainingCurriculumService, TrainingCurriculum);
+  }
+  @Post()
+  @UseGuards(SessionGuard)
+  async createCurriculum(
+    @Res() res,
+    @Body() createCurriculumDTO,
+  ): Promise<any> {
+    try {
+      const curriculum = await this.trainingCurriculumService.createCurriculum(
+        createCurriculumDTO,
+      );
+      return res.status(HttpStatus.OK).send(sanitizeResponseObject(curriculum));
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 }
--- src/modules/training/services/training-curriculum.service.ts
+++ src/modules/training/services/training-curriculum.service.ts
@@ -4,13 +4,44 @@ import { Repository } from 'typeorm';
 import { TrainingCurriculum } from '../entities/training-curriculum.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MaintenanceBaseService } from '../../../core/maintenance/services/base.service';
+import { TrainingUnit } from '../entities/training-unit.entity';
+import { TrainingSection } from '../entities/training-section.entity';
 
 @Injectable()
-export class TrainingCurriculumService extends MaintenanceBaseService<TrainingCurriculum> {
-    constructor(
-        @InjectRepository(TrainingCurriculum)
-        trainingCurriculumRepository: Repository<TrainingCurriculum>,
-    ) {
-        super(trainingCurriculumRepository, TrainingCurriculum);
-    }
+export class TrainingCurriculumService extends MaintenanceBaseService<
+  TrainingCurriculum
+> {
+  constructor(
+    @InjectRepository(TrainingCurriculum)
+    private trainingCurriculumRepository: Repository<TrainingCurriculum>,
+
+    @InjectRepository(TrainingUnit)
+    private trainingUnitRepository: Repository<TrainingUnit>,
+
+    @InjectRepository(TrainingSection)
+    private trainingSectionRepository: Repository<TrainingSection>,
+  ) {
+    super(trainingCurriculumRepository, TrainingCurriculum);
+  }
+
+  async createCurriculum(createCurriculumDTO: any): Promise<TrainingCurriculum> {
+    /*TODO: Add temporary creation of training curriculum for test scenarios */
+    let curriculum = new TrainingCurriculum();
+    Object.keys(createCurriculumDTO).forEach((key) => {
+        curriculum[key] = createCurriculumDTO[key];
+      });
+    curriculum.unit = await this.trainingUnitRepository.findOne({
+      where: { uid: createCurriculumDTO.unit },
+    });
+    curriculum.section = await this.trainingSectionRepository.findOne({
+      where: { uid: createCurriculumDTO.section },
+    });
+
+    const savedcurriculum = await this.trainingCurriculumRepository.save(
+      curriculum,
+    );
+    return await this.trainingCurriculumRepository.findOne({
+      where: { uid: savedcurriculum.uid },
+    });
+  }
 }
