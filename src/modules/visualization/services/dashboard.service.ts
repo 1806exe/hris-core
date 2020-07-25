76e5704113a1bef391f0a6f96d2ca8180883bc61
Bennett <bennybenester@gmail.com>
2020-07-25 16:27:24 +0300

    refactor: add service methods for dashboard sharing

--- src/modules/visualization/services/dashboard.service.ts
+++ src/modules/visualization/services/dashboard.service.ts
@@ -1,17 +1,98 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { BaseService } from '../../../core/services/base.service';
-import { Repository } from 'typeorm';
-
-import { Dashboard } from '../entities/dashboard.entity';
+import { getConnection, Repository } from 'typeorm';
 import { MaintenanceBaseService } from '../../../core/maintenance/services/base.service';
+import { User } from '../../system/user/entities/user.entity';
+import { DashboardAccess } from '../entities/dashboard-useraccess.entity';
+import { Dashboard } from '../entities/dashboard.entity';
 
 @Injectable()
 export class DashboardService extends MaintenanceBaseService<Dashboard> {
   constructor(
     @InjectRepository(Dashboard)
-    repository: Repository<Dashboard>,
+    private dashboardRepository: Repository<Dashboard>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(DashboardAccess)
+    private dashboardAccessRepository: Repository<DashboardAccess>,
   ) {
-    super(repository, Dashboard);
+    super(dashboardRepository, Dashboard);
+  }
+
+  async findOneDashboard(param: any): Promise<any> {
+    return await this.dashboardRepository.findOne({
+      where: { uid: param },
+    });
+  }
+
+  async dashboardSharingCreation(
+    uid: String,
+    dashboardsharingDTO: any,
+  ): Promise<any> {
+    const shareduser = await this.userRepository.findOne({
+      where: { uid: dashboardsharingDTO.user },
+    });
+    const dashboardaccess = new DashboardAccess();
+    const dashboard = await this.dashboardRepository.findOne({
+      where: { uid: uid },
+    });
+    dashboardaccess.userid = (
+      await this.userRepository.findOne({
+        where: { uid: dashboardsharingDTO.user },
+      })
+    ).id;
+
+    dashboardaccess.access = dashboardsharingDTO.access;
+
+    const shareddashboard = await this.dashboardAccessRepository.save(
+      dashboardaccess,
+    );
+    if (shareddashboard !== undefined) {
+      await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into('dashboarduseraccess')
+        .values([
+          {
+            dashboardId: dashboard.id,
+            dashboardaccessId: shareddashboard.id,
+          },
+        ])
+        .execute();
+    }
+    return {
+      message: `Dashboard with ID ${uid} shared with ${shareduser.firstName}`,
+      dashboardaccess: shareddashboard,
+    };
+  }
+  async dashboardSharingEdit(uid: string, editSessionDTO): Promise<any> {
+    const { user, access } = editSessionDTO;
+    const shareduser = await this.userRepository.findOne({
+      where: { uid: editSessionDTO.user },
+    });
+    const userId = (
+      await this.userRepository.findOne({
+        where: { uid: user },
+      })
+    ).id;
+    const accessId = await this.dashboardAccessRepository.findOne({
+      where: { userid: userId },
+    });
+    accessId.access = access;
+    await this.dashboardAccessRepository.save(accessId);
+
+    return {
+      message: `Dashboard with ID ${uid} shared with ${shareduser.firstName}`,
+      dashboardaccess: accessId,
+    };
+  }
+  async SharedUser(uid: string): Promise<any> {
+    const user = (await this.userRepository.findOne({ where: { uid: uid } }))
+      .id;
+
+    const dashboardaccessuser = await this.dashboardAccessRepository.findOne({
+      where: { userid: user },
+    });
+    return dashboardaccessuser;
   }
 }
