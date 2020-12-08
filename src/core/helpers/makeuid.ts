399fee58f8c21a463e2cd33ecf6ea7a595c6de70
Bennett <bennybenester@gmail.com>
2020-12-08 14:53:32 +0300

    feat: add function to generate an array of uids

--- src/core/helpers/makeuid.ts
+++ src/core/helpers/makeuid.ts
@@ -1,9 +1,28 @@
-export function generateUid() {
-	let uidGenerated = '';
-	const characters =
-	  '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	for (let i = 0; i < 13; i++) {
-	  uidGenerated += characters.charAt(Math.floor(Math.random() * characters.length));
+const randomSample = (uidArraySample = [], fn = Math.random) => {
+	if ( uidArraySample.length === 0 ) return;
+	return uidArraySample[Math.round( fn() * (uidArraySample.length - 1) )];
+  };
+  
+  export const generateUid = (limit = 13, fn = Math.random) => {
+	const allowedLetters: any = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"].join( '' );
+	const allowedChars: any = ['0123456789', allowedLetters].join( '' );
+  
+	const generatedId = [randomSample( allowedLetters, fn )];
+  
+	for ( let i = 0; i < limit - 1; i++ ) {
+	  generatedId.push( randomSample( allowedChars, fn ) );
 	}
-	return uidGenerated;
-}
+  
+	return generatedId.join( '' );
+  };
+  
+  export const generateCodes = (numberOfUids: number, limit = 13, fn = Math.random) => {
+	const generatedUids = [];
+  
+	for ( let i = 0; i < numberOfUids; i++ ) {
+	  generatedUids.push( generateUid( limit, fn ) );
+	}
+  
+	return generatedUids;
+  };
+  
